03cb1a7f061f88c76127f0714e02c7d5fba96874
Henistein <henriquejesus20@gmail.com>
2024-06-30 15:57:34 +0100

    refactor and clean

--- main.py
+++ main.py
@@ -1,75 +1,53 @@
 import sys
-from bs4 import BeautifulSoup
-
-depositos_file = sys.argv[1]
-levantamentos_file = sys.argv[2]
-
-with open(depositos_file, 'r') as f:
-    depositos_content = f.read()
-
-with open(levantamentos_file, 'r') as f:
-    levantamentos_content = f.read()
-
-soup = BeautifulSoup(depositos_content, 'html.parser')
-
-# List to store the transactions
-depositos_ammount = []
-depositos_text = []
-# Find all the divs with the class "myAccount_boxTitleContent"
-box_ammount_contents = soup.find_all('div', class_='myAccount_boxTitleContent')
-box_titled_contents = soup.find_all('div', class_='myAccount_boxTitleEnd')
-
-for box in box_ammount_contents:
-    # Extract the amount from the span with class "myAccount_amount"
-    amount_span = box.find('span', class_='myAccount_amount')
-    amount = amount_span.text.strip() if amount_span else None
-    depositos_ammount.append(amount)
-
-for box in box_titled_contents:
-    # Extract the amount from the span with class "myAccount_amount"
-    titled_span = box.find('span', class_='tag_label')
-    titled_text = titled_span.text
-    depositos_text.append(titled_text)
-
-depositos = list(zip(depositos_ammount, depositos_text))
-
-soup = BeautifulSoup(levantamentos_content, 'html.parser')
-
-# List to store the transactions
-levantamentos_ammount = []
-levantamentos_text = []
-# Find all the divs with the class "myAccount_boxTitleContent"
-box_ammount_contents = soup.find_all('div', class_='myAccount_boxTitleContent')
-box_titled_contents = soup.find_all('div', class_='myAccount_boxTitleEnd')
-
-for box in box_ammount_contents:
-    # Extract the amount from the span with class "myAccount_amount"
-    amount_span = box.find('span', class_='myAccount_amount')
-    amount = amount_span.text.strip() if amount_span else None
-    levantamentos_ammount.append(amount)
-
-for box in box_titled_contents:
-    # Extract the amount from the span with class "myAccount_amount"
-    titled_span = box.find('span', class_='tag_label')
-    titled_text = titled_span.text
-    levantamentos_text.append(titled_text)
-
-levantamentos = list(zip(levantamentos_ammount, levantamentos_text))
-
 import re
-soma_depositos = []
-for i in range(len(depositos)):
-    dep = depositos[i]
-    if dep[1] == 'Confirmado':
-        numeric_amount = float(re.search(r'[\d,]+', dep[0]).group().replace(',', '.'))
-        soma_depositos.append(numeric_amount)
+from bs4 import BeautifulSoup
+from enum import Enum
 
-soma_levantamentos = []
-for i in range(len(levantamentos)):
-    dep = levantamentos[i]
-    if dep[1] == 'Confirmado':
-        numeric_amount = float(re.search(r'[\d,]+', dep[0]).group().replace(',', '.'))
-        soma_levantamentos.append(numeric_amount)
+class PaymentType(Enum):
+  DEPOSIT = "deposit"
+  WITHDRAW = "withdrawal" 
 
+data_file = sys.argv[1]
 
-print(sum(soma_depositos) - sum(soma_levantamentos))
+with open(data_file, 'r') as f:
+  data = f.read()
+
+def extract_transactions(soup: BeautifulSoup, type: PaymentType) -> list:
+  tab = soup.find(f'bc-payment-history-{type.value}', class_='w_100 is-active ng-star-inserted')
+  box_ammount_contents = tab.find_all('div', class_='myAccount_boxTitleContent')
+  box_titled_contents = tab.find_all('div', class_='myAccount_boxTitleEnd')
+
+  ammounts, titles = [], []
+
+  for box in box_ammount_contents:
+    # Extract the amount from the span with class "myAccount_amount"
+    amount_span = box.find('span', class_='myAccount_amount')
+    ammounts.append(amount_span.text.strip())
+
+  for box in box_titled_contents:
+    # Extract the title from the span with class "tab_label"
+    titled_span = box.find('span', class_='tag_label')
+    titles.append(titled_span.text)
+
+  return list(zip(ammounts, titles))
+
+def calculate_sum(lst):
+  ret = []
+  for i in range(len(lst)):
+    if lst[i][1] == 'Confirmado':
+      numeric_amount = float(re.search(r'[\d,]+', lst[i][0]).group().replace(',', '.'))
+      ret.append(numeric_amount)
+  return ret
+
+if __name__ == '__main__':
+  soup = BeautifulSoup(data, 'html.parser')
+
+  deposits = extract_transactions(soup, PaymentType.DEPOSIT)
+  withdrawls = extract_transactions(soup, PaymentType.WITHDRAW)
+
+  dep_sum = calculate_sum(deposits)
+  wit_sum = calculate_sum(withdrawls)
+
+  profit = -(sum(dep_sum) - sum(wit_sum))
+
+  print(f'Profit: {round(profit, 2)}€')
